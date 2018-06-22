6bf9a9d694d068aaff235ed69cc8aa952c3bebba
Jhon Villamizar <jhonvio@hotmail.com>
2018-06-21 21:35:40 -0500

    Delete footer.php

--- busca-restaurante/incluidos/footer.php
+++
@@ -1,6 +0,0 @@
-<?php
-/* Script que contiene el footer o remate de cada pagina del proyecto */ 
-?>
-<div class="container">
-<footer>Proyecto Portal web,  Version: 1, <?php echo date("Y");?></footer>
-</div>
