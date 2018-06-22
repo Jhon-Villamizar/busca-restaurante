b65222ce453aaa7a7e1fa95a932425932346d473
Jhon Villamizar <jhon.villamizar.ochoa@gmail.com>
2018-06-22 16:49:04 -0500

    diseño tipos

--- tipos.php
+++ tipos.php
@@ -28,7 +28,7 @@ $datos=$clase->listar();
 	<div class="container">
 		<div class="row">
 			<div class="tipos mb-3">
-			<div class="card mr-3" style="width: 18rem;">
+			<div class="card col-xl-3 col-lg-4 col-md-5 col-xs-3">
 				<img class="card-img-top" src="http://asadacho.com/wp-content/uploads/2015/01/asado-parrilla.jpg" alt="Card image cap" style="height: 200px">
 				<div class="card-body">
 					<h5 class="card-title">Asados</h5>
@@ -36,7 +36,7 @@ $datos=$clase->listar();
 					<a href="#" class="btn btn-outline-light">Lista</a>
 				</div>
 			</div>
-			<div class="card mr-3" style="width: 18rem;">
+			<div class="card col-xl-3 col-lg-4 col-md-5 col-xs-3">
 				<img class="card-img-top" src="http://www.elattelier.com/wp-content/uploads/2015/04/sukiyaki.jpg" alt="Card image cap" style="height: 200px">
 				<div class="card-body">
 					<h5 class="card-title">Oriental</h5>
@@ -45,7 +45,7 @@ $datos=$clase->listar();
 					<a href="#" class="btn btn-outline-light">Lista</a>
 				</div>
 			</div>
-			<div class="card mr-3" style="width: 18rem;">
+			<div class="card col-xl-3 col-lg-4 col-md-5 col-xs-3">
 				<img class="card-img-top" src="http://www.puraitalia.com/wp-content/uploads/2015/06/vinitos.jpg" alt="Card image cap" style="height: 200px">
 				<div class="card-body">
 					<h5 class="card-title">Italiano</h5>
@@ -54,7 +54,7 @@ $datos=$clase->listar();
 					<a href="#" class="btn btn-outline-light">Lista</a>
 				</div>
 			</div>
-			<div class="card mr-3" style="width: 18rem;">
+			<div class="card col-xl-3 col-lg-4 col-md-5 col-xs-3">
 				<img class="card-img-top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8IZzp-oKrw2MHleBV8J5wBvwYABz6K39SYxKdFp4cfaRic77EjQ" alt="Card image cap" style="height: 200px">
 				<div class="card-body">
 					<h5 class="card-title">Mexicano</h5>
@@ -63,12 +63,9 @@ $datos=$clase->listar();
 					<a href="#" class="btn btn-outline-light">Lista</a>
 					</div>
 			</div>
-			</div>
-		</div>		
 		
-		<div class="row">
-			<div class="tipos mb-3">
-			<div class="card mr-3" style="width: 18rem;">
+			
+			<div class="card col-xl-3 col-lg-4 col-md-5 col-xs-3">
 				<img class="card-img-top" src="http://buenomuybueno.com/wp-content/uploads/2017/04/shutterstock_270666803-1.jpg" alt="Card image cap" style="height: 200px">
 				<div class="card-body">
 					<h5 class="card-title">De Mar</h5>
@@ -77,7 +74,7 @@ $datos=$clase->listar();
 					<a href="#" class="btn btn-outline-light">Lista</a>
 				</div>
 			</div>
-			<div class="card mr-3" style="width: 18rem;">
+			<div class="card col-xl-3 col-lg-4 col-md-5 col-xs-3">
 				<img class="card-img-top" src="http://hablemosdeculturas.com/wp-content/uploads/2017/11/comida-tipica-de-colombia-3-1024x576.jpg" alt="Card image cap" style="height: 200px">
 				<div class="card-body">
 					<h5 class="card-title">Tipica</h5>
@@ -85,7 +82,7 @@ $datos=$clase->listar();
 					<a href="#" class="btn btn-outline-light">Lista</a>
 				</div>
 			</div>
-			<div class="card mr-3" style="width: 18rem;">
+			<div class="card col-xl-3 col-lg-4 col-md-5 col-xs-3">
 				<img class="card-img-top" src="http://www.cocinayvino.com/wp-content/uploads/2017/01/comidarapida.jpg" alt="Card image cap" style="height: 200px">
 				<div class="card-body">
 					<h5 class="card-title">Rapida</h5>
@@ -94,18 +91,16 @@ $datos=$clase->listar();
 					<a href="#" class="btn btn-outline-light">Lista</a>
 				</div>
 			</div>
-			<div class="card mr-3" style="width: 18rem;">
+			<div class="card col-xl-3 col-lg-4 col-md-5 col-xs-3">
 				<img class="card-img-top" src="http://cdn2.cocinadelirante.com/sites/default/files/styles/gallerie/public/images/2018/03/recetas-con-cafe-helado.jpg" alt="Card image cap" style="height: 200px">
 				<div class="card-body">
 					<h5 class="card-title">Cafes</h5>
 					<p class="card-text">Lugares amenos para tomar alimentos livianos acompañados de cafe.</p>
+					<br>
 					<a href="#" class="btn btn-outline-light">Lista</a>
 				</div>
 			</div>
-		
 		</div>
-		</div>
-		
 	</div>
 <?php include ("incluidos/js.php");?>
 <script type="text/javascript">
